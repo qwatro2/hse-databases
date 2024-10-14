f55ef37acc9e2d0990e7533b189bc8b7cb3a6f82
qwatro2 <kv22828@gmail.com>
2024-10-15 02:29:11 +0300

    fit: task3/query3

---
+++ hw6/task3/queries/3.sql
@@ -0,0 +1,12 @@
+-- выводит всех студентов и их лучшую оценку, если эта оценки не меньше 4.0
+
+SELECT s.Name, c.Note
+FROM Student s, Lecture lec, "check" c
+WHERE s.MatrNr = c.MatrNr 
+AND lec.LectNr = c.LectNr 
+AND c.Note >= 4
+AND c.Note >= ALL (
+  SELECT c1.Note 
+  FROM "check" c1 
+  WHERE c1.MatrNr = c.MatrNr 
+);
