a934c599c34df1166294de418559edc9056679a3
qwatro2 <kv22828@gmail.com>
2024-10-15 02:37:13 +0300

    fit: task2\query2

---
+++ hw6/task3/queries/2.sql
@@ -0,0 +1,18 @@
+-- выводит номер профессора, его имя и суммарное количество кредитов.
+
+(
+  SELECT p.ProfNr, p.Name, sum(lec.Credit)
+  FROM Professor p, Lecture lec
+  WHERE p.ProfNr = lec.ProfNr
+  GROUP BY p.ProfNr, p.Name
+)
+UNION 
+(
+  SELECT p.ProfNr, p.Name, 0
+  FROM Professor p
+  WHERE NOT EXISTS (
+    SELECT * 
+    FROM Lecture lec 
+    WHERE lec.ProfNr = p.ProfNr 
+  )
+);
