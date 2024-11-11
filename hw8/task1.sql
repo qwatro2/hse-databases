7d75b0e1cdb4ccd3743cce6c46f5f36d0361bd09
qwatro2 <kv22828@gmail.com>
2024-11-11 22:51:18 +0300

    style: remove comments

--- hw8/task1.sql
+++ hw8/task1.sql
@@ -1,4 +1,3 @@
--- point A
 create procedure NEW_JOB(
     job_id varchar(10), 
     job_title varchar(35), 
@@ -9,5 +8,4 @@ as $$
 insert into jobs values (job_id, job_title, min_salary, 2 * min_salary);
 $$;
 
--- point B
 call NEW_JOB('SY_ANAL', 'System Analyst', 6000);
