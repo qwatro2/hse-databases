2122cf6bbcbcad2d2c1662be3945d02e5903d80d
qwatro2 <kv22828@gmail.com>
2024-11-11 23:21:28 +0300

    fit: change language

--- hw8/task1.sql
+++ hw8/task1.sql
@@ -1,11 +1,16 @@
 create procedure NEW_JOB(
-    p_job_id varchar(10), 
-    p_job_title varchar(35), 
+    p_job_id varchar(10),
+    p_job_title varchar(35),
     p_min_salary integer
 )
-language sql
+language plpgsql
 as $$
-insert into jobs values (p_job_id, p_job_title, p_min_salary, 2 * p_min_salary);
+    declare
+        v_max_salary integer;
+    begin
+        v_max_salary = 2 * p_min_salary;
+        insert into jobs values (p_job_id, p_job_title, p_min_salary, v_max_salary);
+    end;
 $$;
 
 call NEW_JOB('SY_ANAL', 'System Analyst', 6000);
